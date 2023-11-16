c75f639502bd96d20b7edd7aaa3f440340887128
Fred Walther <alfred.walther@artingo.net>
2023-11-16 09:40:02 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
+// ToDo: Packages aktualisieren
 Vue.config.productionTip = false
 
 new Vue({
